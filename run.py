7d08d3dea98119771f9dee6fb5228404e1d89746
lishamhlanga <lishamhlanga@gmail.com>
2022-04-14 00:43:45 +0000

    checking for missing weekly data and prompting user to enter new data

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -13,7 +13,18 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('weather')
 
 week = SHEET.worksheet("week1")
-
 data = week.get_all_values()
-
 print(data)
+
+def get_weekly_data():   
+    
+    print("Please enter missing weekly data for week1: ")
+    print("Data should be day, temp, condition separated by commas")
+    print("Example : Monday, 10, Cloudy\n")
+
+    data_str = input ("Please enter missing weekly data:\n")
+    print(f"The data provided is\n {data_str}")
+
+
+get_weekly_data()
+
